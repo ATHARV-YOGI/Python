19201bc28ec257f646d8ff328c45329dd99c3470
Atharv Yogi <atharvyogi123@gmail.com>
2025-08-17 17:00:01 +0530

    commit

---
+++ chapter 9 - ps/1.py
@@ -0,0 +1,23 @@
+import random
+
+def game():
+    print("you are playing game..")
+    score = random.randint(1,23)
+
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore!=""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0
+
+    print(f"your :{score}")
+    if(score>hiscore):
+        with open ("hiscore.txt","w") as f:
+            f.write(str(score))
+
+    return score
+        
+
+
+game()
